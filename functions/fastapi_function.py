43df8494ffe9b2008995dc695ae8e11c7ef5e31c
prithiviraj2804 <prithivi2804raj@gmail.com>
2024-10-19 13:16:53 +0000

    fina

--- functions/fastapi_function.py
+++ functions/fastapi_function.py
@@ -1,6 +1,6 @@
 import os
-from mangum import Mangum  # AWS Lambda to ASGI adapter
-from app.main import app   # Import FastAPI app
+from magnum import Mangum  # AWS Lambda to ASGI adapter
+from run import app   # Import FastAPI app
 
 # Use Mangum to handle requests via AWS Lambda (Netlify Functions)
 handler = Mangum(app)
